11baa7af6eb2274541d12c262b800f0c2d2ab2fd
Yanjun1337 <y_deng1@u.pacific.edu>
2024-09-30 14:43:39 -0700

    MyFirstTimer.java changed. 1. Delay time which controls how often the timer fires 2. ActionListener that defines what happens each time the timer fires 3. start() method, which must be called to begin the timer.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,23 +1,43 @@
 import acm.graphics.*;
 import acm.program.*;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFirstTimer extends GraphicsProgram {
-	public static final int PROGRAM_HEIGHT = 600;
-	public static final int PROGRAM_WIDTH = 800;
-	public static final int MAX_STEPS = 20;
-	private GLabel myLabel;
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
+    public static final int PROGRAM_HEIGHT = 600;
+    public static final int PROGRAM_WIDTH = 800;
+    private GLabel myLabel;
+    private Timer timer;
+    private int numTimes = 0;
+    public static final int STEP_SIZE = 5;
+    public static final int DELAY = 1000;
 
-	public void init() {
-		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
-		requestFocus();
-	}
-	
-	public void run() {
-		myLabel = new GLabel("# of times called?", 0, 100);
-		add(myLabel);
-	}
-	
-	public static void main(String[] args) {
-		new MyFirstTimer().start();
-	}
+    public void init() {
+        setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
+        requestFocus();
+    }
+
+    public void run() {
+        myLabel = new GLabel("Times called: 0", 0, 100);
+        add(myLabel);
+        
+        timer = new Timer(DELAY, this); 
+        timer.start(); 
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        numTimes++;
+        myLabel.setLabel("Times called: " + numTimes);
+        
+        myLabel.move(STEP_SIZE, 0);
+
+        if (numTimes >= 10) {
+            timer.stop();
+        }
+    }
+
+    public static void main(String[] args) {
+        new MyFirstTimer().start();
+    }
 }
